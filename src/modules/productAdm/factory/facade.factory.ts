595ef77b6740354eca2458ff0d5a9cc86b289122
Wesley Praca <simpletextbr@gmail.com>
2023-09-12 21:25:15 -0300

    Atualizando retornos das facades

--- src/modules/productAdm/factory/facade.factory.ts
+++ src/modules/productAdm/factory/facade.factory.ts
@@ -1,10 +1,11 @@
+import IProductAdmFacade from "../facade/IProductAdm";
 import ProductAdmFacade from "../facade/productAdm.facade";
 import ProductRepository from "../repository/product.repository";
 import AddProductUseCase from "../usecase/add/add.product.usecase";
 import CheckStockProductUseCase from "../usecase/checkStock/checkStock.product.usecase";
 
 export default class ProductAdmFacadeFactory {
-  static create() {
+  static create(): IProductAdmFacade {
     const productRepository = new ProductRepository();
     const addProductUsecase = new AddProductUseCase(productRepository);
     const checkStockUsecase = new CheckStockProductUseCase(productRepository);
